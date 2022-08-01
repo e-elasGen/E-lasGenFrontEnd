5f0aad4bc582391b8fe8b239da82560316ee8462
Leonardo luis rampazi <105321172+Leonardo-Rampazi@users.noreply.github.com>
2022-08-01 12:27:33 -0300

    criando todas as páginas

---
+++ e-lasgenfrontend/src/paginas/home/Home.tsx
@@ -0,0 +1,30 @@
+import React from 'react';
+import { Typography, Grid, Button } from '@material-ui/core';
+import {Box} from '@mui/material'
+import './Home.css';
+
+function home() {
+    return (
+        <>
+        <Grid container direction="row" justifyContent="center" alignItems="center" className='caixa estilo-grid' >
+        <Grid alignItems="center" item xs={6}>
+            <Box paddingX={20} >
+                <Typography variant="h3" gutterBottom color="textPrimary" component="h3" align="center" className='titulo'>Seja bem vindo(a)! a e-lasGen</Typography>
+                <Typography variant="h5" gutterBottom color="textPrimary" component="h5" align="center" className='titulo'>Um ecommerce que almeja ser um espaço de inclusão para mulheres que se encontram em vulnerabilidade econômica/social</Typography>
+            </Box>
+            <Box display="flex" justifyContent="center">
+                <Box marginRight={1}>
+                </Box>
+                <Button variant="outlined" className='botao'>Ver e-commerce</Button>
+            </Box>
+        </Grid>
+        <Grid item xs={6} >
+            <img src="https://i.imgur.com/7eETai7.jpeg" alt="" className='img'/>
+        </Grid>
+        <Grid xs={12} className='postagens'>
+        </Grid>
+    </Grid>
+</>
+    );
+}
+export default home;
