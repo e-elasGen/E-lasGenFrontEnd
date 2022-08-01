a248351807f1ca414e7a94e9569e4a9e36a85b03
Vanessa Basilio Jardim <vanessajardim063013@gmail.com>
2022-08-01 12:18:54 -0300

    tesk estaticos

--- e-lasgenfrontend/src/App.tsx
+++ e-lasgenfrontend/src/App.tsx
@@ -1,9 +1,14 @@
 import React from 'react';
 import './App.css';
+import Navbar from './estaticos/navbar/Navbar'
+import Footer from './estaticos/footer/Footer'
 
 function App() {
   return (
-    <>Elas gen</>
+    <>
+    Elas gen
+    
+    </>
     
   )
 }
