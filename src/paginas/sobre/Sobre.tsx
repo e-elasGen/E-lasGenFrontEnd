cdb4f55c82054e68a4a3d36c7c670c3decc47a67
Manoela <manoelamelo24@gmail.com>
2022-08-04 15:58:59 -0300

    Sobre

--- src/paginas/sobre/Sobre.tsx
+++ src/paginas/sobre/Sobre.tsx
@@ -1,10 +1,21 @@
 import React from "react";
 import './Sobre.css';
-function sobre(){
-    return(
-    <>
-    <h1>Sobre nós e-lasGen !</h1>
-    </>
+function sobre() {
+    return (
+        <>
+
+            <h1>E-lasGen !</h1>
+            <h2> Objetivo </h2>
+            <p>A crise sanitária da COVID-19 tem resultado em uma queda substancial no emprego em diversos países do mundo, e os impactos têm se mostrado ainda mais acentuados para mulheres. Os desafios para ampliar a participação feminina no mercado de trabalho – que já eram significativos antes da pandemia – tornaram-se ainda mais críticos no contexto de crise que vivemos, fazendo com que países como o Brasil cheguem a patamares históricos que configuram retrocessos de mais de 30 anos. </p>
+
+
+            <p> Nosso projeto visa movimentar a economia para mulheres, buscando incentivar o comércio e, com isso, aumentar e/ou gerar renda para mulheres que são responsáveis pelo sustento de suas familia e que se encontram em situação de instabilidade financeira devido a crise econômica que estamos vivendo no período “pós” pandemia.</p>
+            <h2>Sobre nós</h2>
+            <p>Projeto final do bootcamp idealizado pela Generation Brasil, utilizando de técnicas de programação, soft skills e metodologias ágeis para construção dessa aplicação.
+
+            </p>
+
+        </>
     );
-    }
-    export default sobre;
+}
+export default sobre;
